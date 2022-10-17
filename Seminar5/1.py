c3748705b3537754833d6372fcd8c7b418700cbd
cabyrc2910 <cabyrc@list.ru>
2022-10-17 21:43:09 +0300

    Сохранение

--- Seminar5/1.py
+++ Seminar5/1.py
@@ -1,8 +1,12 @@
-# Напишите программу, удаляющую из текста все слова, содержащие ""abc"".
+###################################################################
+# В файле находится N натуральных чисел, записанных через пробел.
+# Среди чисел не хватает одного, чтобы выполнялось условие
+# A[i] - 1 = A[i-1]. Найдите это число.
+##################################################################
 
-input_string = "sdfsdf abc asdf asdfabc sdfsabc asdfsdfasd ljklkj asdfsadf"
-remove_word = "abc"
-
-temp_list = list(filter(lambda x: remove_word not in x, input_string.split()))
-
-print(" ".join(temp_list))
+my_str = '1 2 3 4 5 6 8 9'
+my_list = list(map(int, my_str.split()))
+print(my_list)
+for i in range(1, len(my_list)):
+    if my_list[i]-1 != my_list[i-1]:
+        print(my_list[i-1]+1)
--- Seminar5/3.py
+++ Seminar5/3.py
@@ -1,1 +1,17 @@
-#  
+#############################################################################
+# Напишите программу, удаляющую из текста все слова, содержащие ""abc"".
+#############################################################################
+
+input_string = "sdfsdf abc asdf asdfabc sdfsabc asdfsdfasd ljklkj asdfsadf"
+remove_word = "abc"
+
+temp_list = list(filter(lambda x: remove_word not in x, input_string.split()))
+print(" ".join(temp_list))
+
+#############################
+
+text_my = 'Напишите программуabc , удаляющуюabc из текста все словаabc, содержащие "abc"'
+
+text_my = list(filter(lambda x: 'abc' not in x, text_my.split()))
+my = " ".join(text_my)
+print(my)
