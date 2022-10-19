28ffb55eb134828027b60b041415919a02024f8b
cabyrc2910 <cabyrc@list.ru>
2022-10-19 19:28:39 +0300

    Сохранение

--- Seminar4/ДЗ.task2.py
+++ Seminar4/ДЗ.task2.py
@@ -3,16 +3,16 @@
 
 ###########################################################
 
-N = 54
-my_list = []
-factor = 2
-while N > 1:
-    if N % factor == 0:
-        my_list.append(factor)
-        N = N / factor
-    else:
-        factor += 1
-print(my_list)
+# N = 54
+# my_list = []
+# factor = 2
+# while N > 1:
+#     if N % factor == 0:
+#         my_list.append(factor)
+#         N = N / factor
+#     else:
+#         factor += 1
+# print(my_list)
 
 #############################################################
 
@@ -31,3 +31,18 @@ print(my_list)
 #     else:
 #         i += 1
 # print(f"\nПростые множители числа {num} приведены в списке: {new_list}")
+
+########################################################################
+
+n = int(input('Введите число: '))
+lst = []
+for i in range(2, n**0,5):
+    if n % i == 0:
+        count = 1
+        for j in range(2, (i/2 + 1)):
+            if (i % j == 0):
+                count = 0
+                break
+        if (count == 1):
+            lst.append(i)
+print(lst)
